9ea12ca8e5802f1ac53aa5c486c639a6e5a855e6
gjf2a <ferrer@hendrix.edu>
2022-03-17 11:07:01 -0500

    Added a function to run the planner but not from the command line.

--- pyhop_anytime/auxiliary.py
+++ pyhop_anytime/auxiliary.py
@@ -5,18 +5,31 @@ def anyhop_main(planner):
     if len(sys.argv) == 1:
         print(f"Usage: python3 {sys.argv[0]} -v:[verbosity] -s:[max seconds] [planner_file]+")
     else:
-        verbosity = 1
-        max_seconds = None
-        for filename in sys.argv[1:]:
-            if filename.startswith("-v"):
-                verbosity = int(filename.split(':')[1])
-            elif filename.startswith("-s"):
-                max_seconds = float(filename.split(':')[1])
-            else:
-                exec(open(filename).read())
-                plans = planner.anyhop(state, [('start', goals)], max_seconds=max_seconds, verbose=verbosity)
-                for (plan, time) in plans:
-                    print(plan)
-                for (plan, time) in plans:
-                    print(f"Length: {len(plan)} time: {time}")
-                print(len(plans), "total plans generated")
+        anyhop_args(planner, sys.argv[1:])
+
+
+def anyhop_args(planner, args):
+    verbosity = find_verbosity(args)
+    max_seconds = find_max_seconds(args)
+    for filename in args:
+        if not filename.startswith("-"):
+            exec(open(filename).read())
+            plans = planner.anyhop(state, [('start', goals)], max_seconds=max_seconds, verbose=verbosity)
+            for (plan, time) in plans:
+                print(plan)
+            for (plan, time) in plans:
+                print(f"Length: {len(plan)} time: {time}")
+            print(len(plans), "total plans generated")
+
+
+def find_verbosity(args):
+    for arg in args:
+        if arg.startswith("-v"):
+            return int(arg.split(':')[1])
+    return 1
+
+
+def find_max_seconds(args):
+    for arg in args:
+        if arg.startswith("-s"):
+            return int(args.split(':')[1])
