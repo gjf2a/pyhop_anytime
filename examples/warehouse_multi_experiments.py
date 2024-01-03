37248e8146a7fadc550d4b5773d88ce928549585
gjf2a <ferrer@hendrix.edu>
2024-01-03 11:46:51 -0600

    Since single-robot warehouse is not interesting, I deleted those files.
    I augmented multi-robot warehouse to allow for command-line variation in experiment parameters.

--- examples/warehouse_multi_experiments.py
+++ examples/warehouse_multi_experiments.py
@@ -1,11 +1,20 @@
 from warehouse_multi import *
 from pyhop_anytime.pyhop import *
+import sys
 
 if __name__ == '__main__':
+    num_packages = 3
+    num_robots = 2
+    for arg in sys.argv:
+        if arg.startswith("-robot"):
+            num_robots = int(arg.split(":")[1])
+        elif arg.startswith("-packages"):
+            num_packages = int(arg.split(":")[1])
+
     planner = warehouse_planner()
     for i in range(10):
         print(f"Experiment {i + 1}")
-        state, tasks = problem_generator(10, 10, 3, 2)
+        state, tasks = problem_generator(10, 10, num_packages, num_robots)
 
         print("purely random")
         plan_times = planner.anyhop_random(state, tasks, max_seconds=4)
@@ -15,8 +24,14 @@ if __name__ == '__main__':
         plan_times = planner.anyhop(state, tasks, max_seconds=4)
         print([(plan[1], plan[2]) for plan in plan_times])
         print()
+        print("monte carlo go_deep")
+        plan_times = planner.anyhop(state, tasks, max_seconds=4,
+                                    queue_init=lambda: MonteCarloPlannerHeap(planner, go_deep_first=True))
+        print([(plan[1], plan[2]) for plan in plan_times])
+        print()
         print("monte carlo")
-        plan_times = planner.anyhop(state, tasks, max_seconds=4, queue_init=lambda: MonteCarloPlannerHeap(planner))
+        plan_times = planner.anyhop(state, tasks, max_seconds=4,
+                                    queue_init=lambda: MonteCarloPlannerHeap(planner, go_deep_first=False))
         print([(plan[1], plan[2]) for plan in plan_times])
         print()
         print()
