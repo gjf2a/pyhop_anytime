42a59dc5ed4363bfd937b9e7156332a790578d99
gjf2a <ferrer@hendrix.edu>
2024-01-03 11:53:18 -0600

    Also added a max_second variation parameter.

--- examples/warehouse_multi_experiments.py
+++ examples/warehouse_multi_experiments.py
@@ -3,13 +3,16 @@ from pyhop_anytime.pyhop import *
 import sys
 
 if __name__ == '__main__':
-    num_packages = 3
-    num_robots = 2
+    num_packages = 4
+    num_robots = 3
+    max_seconds = 4
     for arg in sys.argv:
         if arg.startswith("-robot"):
             num_robots = int(arg.split(":")[1])
         elif arg.startswith("-packages"):
             num_packages = int(arg.split(":")[1])
+        elif arg.startswith("-seconds"):
+            max_seconds = int(arg.split(":")[1])
 
     planner = warehouse_planner()
     for i in range(10):
@@ -17,20 +20,20 @@ if __name__ == '__main__':
         state, tasks = problem_generator(10, 10, num_packages, num_robots)
 
         print("purely random")
-        plan_times = planner.anyhop_random(state, tasks, max_seconds=4)
+        plan_times = planner.anyhop_random(state, tasks, max_seconds=max_seconds)
         print([(plan[1], plan[2]) for plan in plan_times])
         print()
         print("DFS")
-        plan_times = planner.anyhop(state, tasks, max_seconds=4)
+        plan_times = planner.anyhop(state, tasks, max_seconds=max_seconds)
         print([(plan[1], plan[2]) for plan in plan_times])
         print()
         print("monte carlo go_deep")
-        plan_times = planner.anyhop(state, tasks, max_seconds=4,
+        plan_times = planner.anyhop(state, tasks, max_seconds=max_seconds,
                                     queue_init=lambda: MonteCarloPlannerHeap(planner, go_deep_first=True))
         print([(plan[1], plan[2]) for plan in plan_times])
         print()
         print("monte carlo")
-        plan_times = planner.anyhop(state, tasks, max_seconds=4,
+        plan_times = planner.anyhop(state, tasks, max_seconds=max_seconds,
                                     queue_init=lambda: MonteCarloPlannerHeap(planner, go_deep_first=False))
         print([(plan[1], plan[2]) for plan in plan_times])
         print()
