c04eef843b96ebde73b1d367c1a8bee9fa419a97
gjf2a <ferrer@hendrix.edu>
2024-01-17 21:12:12 -0600

    Added more functions.

--- pyhop_anytime/stats.py
+++ pyhop_anytime/stats.py
@@ -8,6 +8,16 @@ def mean_and_bound(values: List[float], num_stdevs: int) -> Tuple[float, float]:
     return xbar, bound
 
 
-def report_mean_and_bound(values: List[float], num_stdevs: int) -> str:
+def report_mean_bound(values: List[float], num_stdevs: int) -> str:
     xbar, bound = mean_and_bound(values, num_stdevs)
     return f"{xbar:.2f} +/- {bound:.2f}"
+
+
+def lo_hi_bounds(values: List[float], num_stdevs: int) -> Tuple[float, float]:
+    xbar, bound = mean_and_bound(values, num_stdevs)
+    return xbar - bound, xbar + bound
+
+
+def report_lo_hi_bounds(values: List[float], num_stdevs: int) -> str:
+    lo, hi = lo_hi_bounds(values, num_stdevs)
+    return f"({lo:.2f}, {hi:.2f})"
