2a07c694f908edc901d5b8b6468ccf1f444ebf57
gjf2a <ferrer@hendrix.edu>
2024-01-17 20:49:05 -0600

    Added string formatting.

--- pyhop_anytime/stats.py
+++ pyhop_anytime/stats.py
@@ -2,7 +2,12 @@ from typing import List, Tuple
 import statistics
 
 
-def mean_and_bounds(values: List[float], num_stdevs: int) -> Tuple[float, float]:
+def mean_and_bound(values: List[float], num_stdevs: int) -> Tuple[float, float]:
     xbar = statistics.mean(values)
     bound = num_stdevs * statistics.stdev(values, xbar)
     return xbar, bound
+
+
+def report_mean_and_bound(values: List[float], num_stdevs: int) -> str:
+    xbar, bound = mean_and_bound(values, num_stdevs)
+    return f"{xbar} +/- {bound}"
